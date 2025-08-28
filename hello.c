1fe70cff434ab2a010cd9f3b157567ee4312875b
Chloe-C-H <chogan8@huskers.unl.edu>
2025-08-28 12:41:21 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Aiden Kimmerling, Chloe Hogan
+ * Date: 2025/8/28
+ *
+ * A simple name program in C
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chloe Hogan\n");
+
+  return 0;
+}
